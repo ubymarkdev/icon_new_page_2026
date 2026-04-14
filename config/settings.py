d390f094b7592db46d30a70de031ca917bb3a3cc
uby2 <haxfin@gmail>
2026-04-14 15:27:54 -0600

    icon_new_page_prueba v1 add venv debug

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!&01n9(me5^q&wa627zmc_w@g-i5)mq^-%3m&w12bh8jm7i$nd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["icon_new_page_2026.onrender.com"]
 
 
 # Application definition
