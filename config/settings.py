85e42c283f84bb61db5c5de2a641d9d3f8bbe56f
uby2 <haxfin@gmail>
2026-04-15 15:57:05 -0600

    fix cloudinary storage 2

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 print("CLOUDINARY_URL:", os.getenv("CLOUDINARY_URL"))
 
 STORAGES = {
