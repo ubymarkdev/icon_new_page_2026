c592b4bca955d36e1e6e95b561ea1a175952ac38
uby2 <haxfin@gmail>
2026-04-15 15:20:05 -0600

    Cloudinary debug

--- config/settings.py
+++ config/settings.py
@@ -130,5 +130,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+print("CLOUDINARY_URL:", os.getenv("CLOUDINARY_URL"))
 
 PHONENUMBER_DEFAULT_REGION = "MX"
