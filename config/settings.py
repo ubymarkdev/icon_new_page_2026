1bf25e6551fcb63e27330db02c59e8843ed796f6
uby2 <haxfin@gmail>
2026-04-15 14:42:53 -0600

    Integración Cloudinary

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'formulario',
     'cosentino',
     'phonenumber_field',
+    'cloudinary',
+    'cloudinary_storage',
     ]
 
 MIDDLEWARE = [
@@ -122,10 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 PHONENUMBER_DEFAULT_REGION = "MX"
