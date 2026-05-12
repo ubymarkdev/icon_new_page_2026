ba7c0426b937cdbc5c040f90d2c50fbcb3c2e584
uby2 <haxfin@gmail>
2026-05-12 15:06:16 -0600

    form fix

--- formulario/views.py
+++ formulario/views.py
@@ -1,41 +1,42 @@
-from django.shortcuts import render, redirect
+﻿from django.shortcuts import render, redirect
 from urllib.parse import quote
 from .forms import leadForm
 import random
 
+
 # Create your views here.
-def pagina_formulario (request):
+def pagina_formulario(request):
     form = leadForm()
     return render(request, "formulario/formulario.html", {"form": form})
 
 
-def contacto_form (request):
+def contacto_form(request):
     if request.method == "POST":
         form = leadForm(request.POST)
 
         if form.is_valid():
             lead = form.save()
-
             canal = lead.canal
 
-            if canal == "transformador" or "marmolista" or "distribuidor" or "constructora" or "inmobiliaria": 
+            if canal in {"transformador", "marmolero", "distribuidor", "constructora", "inmobiliaria"}:
                 numero = "3315848177"
-            elif canal == "carpintero" or "cocinista" or "mueblero":
+            elif canal in {"carpintero", "cocinista", "mueblero"}:
                 numero = "3318103604"
-            elif canal == "arquitecto" or "ingeniero":
-                numero = "3324493433" 
+            elif canal in {"arquitecto", "ingeniero"}:
+                numero = "3324493433"
             elif canal == "cliente_particular":
-                numeros_particular = ["3315848177","3318103604","3324493433",]
+                numeros_particular = ["3315848177", "3318103604", "3324493433"]
                 numero = random.choice(numeros_particular)
+            else:
+                numero = "3315848177"
+
             mensaje = quote(
                 f"Hola, soy {lead.nombre} de la empresa {lead.empresa} y quiero información sobre {lead.requerimiento}"
             )
-
             url = f"https://wa.me/{numero}?text={mensaje}"
-
             return redirect(url)
 
     else:
         form = leadForm()
 
-    return render(request, "formulario/formulario.html", {"form": form})        
+    return render(request, "formulario/formulario.html", {"form": form})
